e10e8ab19b43812689c0afba8736bc9625a6348a
Moulton <moulton0@gmail.com>
2018-04-05 20:40:35 -0700

    Fixed mistake in heroes component

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,13 +9,13 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  }
-
-  heroes = HEROES;
+  }  
 
   constructor() { }
 
